8f29648531a24ed7411cfecb21d960ae6bef5987
zhkugh <zhkugh@163.com>
2019-12-24 11:45:27 +0800

    update .travis file

--- tests/.travis.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `travis` (
-   `id` INT(11) NOT NULL AUTO_INCREMENT,
-   `content` text,
-    PRIMARY KEY (`id`)
-)
-    COLLATE='utf8_general_ci'
-    ENGINE=InnoDB
-    AUTO_INCREMENT=1
-;
--- tests/EpdoTest.php
+++ tests/EpdoTest.php
@@ -15,6 +15,22 @@ use Zhkugh\Epdo\DB;
 
 class EpdoTest extends TestCase
 {
+    public function test_create_table()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS `travis` (
+                   `id` INT(11) NOT NULL AUTO_INCREMENT,
+                   `content` text,
+                    PRIMARY KEY (`id`)
+                )
+                    COLLATE='utf8_general_ci'
+                    ENGINE=InnoDB
+                    AUTO_INCREMENT=1
+                ;";
+
+        $this->assertSame(1, DB::query($sql) ? 1 : 0);
+    }
+
+
     public function test_insert()
     {
         $rowCount = DB::insert('travis', [
